2995bd6e31571beb8ecd5e8e5ec40eb947e2514e
squarlhan@gmail.com <squarlhan@gmail.com@05791e1a-2341-11de-b851-79cad7b500d3>
2009-04-09 08:38:18 +0000

    可以动态利用拼音取到数据了，但是不弹出下拉框，还得摁一下按钮，加了一个包叫pinyin4j-2.5.0.jar

--- Server/src/com/dcsh/market/action/ajaxaction/GetCustomersAction.java
+++ Server/src/com/dcsh/market/action/ajaxaction/GetCustomersAction.java
@@ -4,6 +4,13 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 import com.dcsh.market.Custom;
 import com.dcsh.market.service.XiaoshouService;
 import com.dcsh.market.service.XiaoshouServiceImpl;
@@ -54,19 +61,46 @@ public class GetCustomersAction implements Action{
 
 	public String execute() {
 		System.out.println("Enter Execute");
+		
+		HanyuPinyinOutputFormat opf = new HanyuPinyinOutputFormat();
+		opf.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+		opf.setToneType(HanyuPinyinToneType .WITHOUT_TONE);
+		opf.setVCharType(HanyuPinyinVCharType.WITH_V);
+		
+		
 		customers = new ArrayList();
 		names = new ArrayList();
 		allcustomers = this.service.getAllCustom();
 		if(start == null || "".equals(start.trim())) {
             start = "a";
-            System.out.println("ssssss£º"+start);
+           
         }
+		System.out.println("xxxxxxx£º"+start);
+
 		for(Custom c:allcustomers){
-			if(c.getCustomName().toLowerCase().startsWith(start.toLowerCase())) {
+			String[] pinyin = null;
+			String result = "";
+			char[] temp = c.getCustomName().toCharArray();
+			try {
+				for(char cc:temp){
+					if((cc>='0'&&cc<='9')||(cc>='a'&&cc<='z')||(cc>='A'&&cc<='Z')){
+						result+=cc;
+					}else{
+		                 pinyin = PinyinHelper.toHanyuPinyinStringArray(cc,opf);
+		                 result+=pinyin[0];     
+					}
+				}
+				//System.out.println("rrrrrrr"+result);
+					     
+			} catch (BadHanyuPinyinOutputFormatCombination e1) {
+				e1.printStackTrace();
+			}
+			
+			if(result.startsWith(start.toLowerCase())) {
 				customers.add(c);
 				names.add(new String[]{c.getCustomName(),String.valueOf(c.getId())});
-				//names.add(new String[]{new String(c.getCustomName().getBytes("iso8859-1"),"gb2312"),String.valueOf(c.getId())});
             }
+			
 		}
 		for(Custom c:customers){
 			System.out.println("kkkkkkkkk£º"+c.getCustomName());
